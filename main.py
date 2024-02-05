d5e6e10507cabaf395fe39986d29da2dc4c3f8b0
dsol28 <j89389333@gmail.com>
2024-02-05 17:57:30 +0300

    вторая задача

--- main.py
+++ main.py
@@ -4,30 +4,38 @@ import sys
 import pygame
 import requests
 
-map_request = "https://static-maps.yandex.ru/1.x/?ll=43.462467,56.248144&z=18&l=map"
-response = requests.get(map_request)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+zxc = 10
+running = True
 
 # Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-# Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+def req():
+    map_request = f"https://static-maps.yandex.ru/1.x/?ll=43.462467,56.248144&z={zxc % 21}&l=map"
+    response = requests.get(map_request)
 
-# Удаляем за собой файл с изображением.
-os.remove(map_file)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+req()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                zxc += 1
+                req()
+            if event.key == pygame.K_DOWN:
+                zxc -= 1
+                req()
+pygame.quit()
