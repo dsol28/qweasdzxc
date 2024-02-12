445e39765afe5174a7ac161814d3a5d4b05d5f2d
dsol28 <j89389333@gmail.com>
2024-02-12 18:58:35 +0300

    Отбор астронавтов

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,6 +17,15 @@ def promotion():
 @app.route('/promotion_image')
 def promotion_image():
     return render_template("index2.html")
+@app.route('/asronaut_selection', methods=['GET', 'POST'])
+def asronaut_selection():
+    if request.method == 'GET':
+        return render_template("index3.html")
+    elif request.method == 'POST':
+        for key, value in request.form.items():
+            print(value)
+        return render_template("index3.html")
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
